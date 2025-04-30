b76577e712633f73571b0ac30b25ded57ef33d81
Alex Lee <alee262@jhmi.edu>
2025-04-30 19:12:08 -0400

    add coordinates to igraph object

--- R/SpotGraph.R
+++ R/SpotGraph.R
@@ -33,19 +33,25 @@
 #' #   geom_edges() +
 #' #   geom_nodes()
 
-SpotGraph = function(coord, max.dist = NULL, cluster = F, resolution = 0.5) {
+SpotGraph = function(coord, dist.buffer = 1.05, max.dist = NULL,
+                     cluster = F, resolution = 0.5) {
   # Get coordinates and calculate euclidean distance
   d = dist(coord, method = 'euclidean')
   m = as.matrix(d)
 
-  #Add x y coordinates to igraph object
-
   # Find the distance between two adjacent spots
-  # - max.dist buffered by hypotenuse if max.dist is not provided by user
-  if (is.null(max.dist)) max.dist = sqrt(2*min(d)^2)
+  # - buffer the min.dist to define adjacency by dist.buffer (default 5%)
+  # - buffer the max.dist by hypotenuse if max.dist is not provided by user
+  # - a spot that is immediately adjacent needs to be within min.dist and max.dist
+  min.dist = min(d)
+  if (is.null(max.dist)) max.dist = sqrt(2*min.dist^2)*dist.buffer
 
   # Create igraph object from edge data frame
   ig = igraph::graph_from_adjacency_matrix(m <= max.dist, mode = "undirected", diag = F)
+
+  # Add x,y coordinates to igraph object
+  igraph::V(ig)$coord_x = coord[,1]
+  igraph::V(ig)$coord_y = coord[,2]
 
   # identify boundary nodes
   is_boundary = degree(ig) < max(degree(ig))
