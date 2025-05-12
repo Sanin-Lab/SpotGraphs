ac0ba9643f71b42f0cfc685a932de419794bfd7c
Alex Lee <alee262@jhmi.edu>
2025-05-12 15:44:02 -0400

    add tests for CleanSlide

--- tests/testthat/test-CleanSlide.R
+++ tests/testthat/test-CleanSlide.R
@@ -1,3 +1,11 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("SpotGraph returns an igraph object", {
+  df = rbind(expand.grid(1:5, 1:5), expand.grid(9:11, 9:11))
+  colnames(df) = c('x', 'y')
+  expect_equal(class(SpotGraph(df)), 'igraph')
+})
+
+test_that("density calculation results in a number in the range of nCount", {
+  nCount = rbinom(20, 100, 0.5)
+  thres = get_threshold(nCount)
+  expect_true(thres > 0 & thres < max(nCount-min(nCount)))
 })
