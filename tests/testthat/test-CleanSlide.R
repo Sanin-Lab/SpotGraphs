dab3c930bf678e54cbd0019dfe6c129f977b8603
Alex Lee <alee262@jhmi.edu>
2025-07-20 23:55:43 -0400

    update cleanslide tests

--- tests/testthat/test-CleanSlide.R
+++ tests/testthat/test-CleanSlide.R
@@ -9,3 +9,15 @@ test_that("density calculation returns a number within the range of nCount", {
   thres = get_threshold(nCount)
   expect_true(thres > 0 & (thres-min(nCount) < max(nCount)-min(nCount)))
 })
+
+test_that("CleanSlide produces expected output", {
+  coord = Seurat::GetTissueCoordinates(scc_s1)[,c('x', 'y')]
+  nCount = scc_s1$nCount_Spatial
+  res = CleanSlide(coord = coord, nCount = nCount)
+
+  res_expected = readRDS(test_path('fixtures', 'cleanslide_expected.rds'))
+
+  expect_snapshot(
+    waldo::compare(res_expected, res)
+  )
+})
